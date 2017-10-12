d6426ebe272a25fdda88888dd3f6426d39ab2805
Joeri Smits <joeri.smits@planet.nl>
2017-10-12 16:52:57 +0200

    Fixed showCase

---
+++ js/quoteSwitcher.js
@@ -0,0 +1,19 @@
+(() => {
+  const triggers = $('#quoteSwitcher').children();
+  let currentPosition = 1;
+
+  let interval = setInterval(() => {
+    if(currentPosition >= triggers.length) {
+      currentPosition = 0;
+    }
+    $(triggers[currentPosition]).trigger('click');
+    currentPosition++;
+  }, 5000);
+
+  triggers.on('click', (e) => {
+    if(e.hasOwnProperty('originalEvent')) {
+      // Only clear on a user click
+      clearInterval(interval);
+    }
+  });
+})();
